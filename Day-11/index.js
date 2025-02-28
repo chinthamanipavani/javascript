71f71f11e9531ee73dba492c0a9d3b2f2552376b
pavani <162785057+chinthamanipavani@users.noreply.github.com>
2025-02-28 22:28:54 +0530

    Update index.js

--- Day-11/index.js
+++ Day-11/index.js
@@ -32,7 +32,10 @@ console.log(calculate(10,5,(num1,num2)=>{
 //Q3
 
 function checkEvenOdd(num, callback){
-    return callback(num)
+   if(callback(num)=='even'){
+      return "even"
+}
+   return "odd"
 }
 console.log(checkEvenOdd(11, (num)=>{
     if(num%2==0){
@@ -55,9 +58,8 @@ console.log(checkSign((-5),(num)=>{
     else if(num<0){
         return "Negative"
     }
-    else{
         return "Zero"
-    }
+    
 }))
 
 //Q5
@@ -67,12 +69,12 @@ function multiplyConditionally(num1, num2, callback){
 }
 console.log(multiplyConditionally(2,-4,(num1,num2)=>{
     if(num1>num2){
-        return num1*num2
+        return num1*num2*4
     }
     else if(num1<num2){
-        return num1+num2
+        return num1+num2*4
     }
     else{
-        return num1
+        return num1*4
     }
 }))
